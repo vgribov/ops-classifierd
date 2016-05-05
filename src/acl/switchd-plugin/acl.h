OpenSwitchNOS/openswitch-ops-classifierd

new: dev: ACL switchd feature plugin
This commit has following changes
- all ACL switchd plugin changes moved under
  classifierd repo

Change-Id: If8e0d957c5b4c35bb63868ce5f4de8f61083f2ce
Signed-off-by: Sandip Bidaye <sandip.bidaye@hpe.com>

src/acl/switchd-plugin/acl.h
@@ -1,0 +1,60 @@
+/*
+ * Copyright (C) 2016 Hewlett Packard Enterprise Development LP
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at:
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef __SWITCHD__PLUGIN__ACL_H__
+#define __SWITCHD__PLUGIN__ACL_H__ 1
+
+#include <stdbool.h>
+#include "hmap.h"
+#include "uuid.h"
+#include "list.h"
+#include "ops-cls-asic-plugin.h"
+#include "reconfigure-blocks.h"
+
+struct classifier_list;
+struct ops_cls_list;
+
+/*************************************************************
+ * acl structures
+ *************************************************************/
+struct acl {
+    struct hmap_node   all_node_uuid;   /* In 'all_acls_by_uuid'. */
+
+    /* members with information "about" me */
+    struct uuid        uuid;
+    const char        *name;
+    enum ops_cls_type  type;
+
+    /* members for working with OVSDB */
+    const struct ovsrec_acl *ovsdb_row;
+    unsigned int       delete_seqno; /* mark/sweep to identify deleted */
+
+    /* members represending my cached PI state */
+    struct ovs_list acl_port_map;    /* List of struct p2acls. */
+    struct ops_cls_list *want_pi; /* temporary until Change system in place */
+};
+
+/*************************************************************
+ * acl lookup routines
+ *************************************************************/
+struct acl* acl_lookup_by_uuid(const struct uuid* uuid);
+
+/************************************************************
+ * Top level routine to check if ACL's need to reconfigure
+ ************************************************************/
+void acl_reconfigure_init(struct blk_params *blk_params);
+
+#endif  /* __SWITCHD__PLUGIN__ACL_H__ */

src/acl/switchd-plugin/acl_ofproto.c
@@ -1,0 +1,190 @@
+/*
+ * Copyright (C) 2016 Hewlett Packard Enterprise Development LP
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at:
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "acl_ofproto.h"
+#include <errno.h>
+#include <config.h>
+#include "plugin-extensions.h"
+#include "ops-cls-asic-plugin.h"
+#include "ofproto/ofproto-provider.h"
+#include "openvswitch/vlog.h"
+#include "acl.h"
+#include "acl_plugin.h"
+#include "acl_log.h"
+
+VLOG_DEFINE_THIS_MODULE(acl_switchd_plugin_ofproto);
+
+static struct plugin_extension_interface *extension = NULL;
+static struct ops_cls_plugin_interface *plugin = NULL;
+
+const char * const ops_cls_type_strings[] = {
+    "INVALID",
+    "ACL_V4",
+    "ACL_V6"
+};
+
+const char * const ops_cls_direction_strings[] = {
+    "INVALID",
+    "IN",
+    "OUT"
+};
+
+struct ops_cls_list*
+ops_cls_list_new(void)
+{
+    struct ops_cls_list *list = xzalloc(sizeof *list);
+    return list;
+}
+
+void
+ops_cls_list_delete(struct ops_cls_list *list)
+{
+    if (list) {
+        free(CONST_CAST(char*, list->list_name));
+        free(list->entries);
+        free(list);
+    }
+}
+
+/* TODO: Access classifier list routines via ofproto extension
+         instead of directly through ofproto_class */
+int
+call_ofproto_ops_cls_apply(struct acl                     *acl,
+                           struct port                    *bridgec_port,
+                           struct ofproto                 *ofproto,
+                           struct ops_cls_interface_info  *interface_info,
+                           enum ops_cls_direction         direction,
+                           struct ops_cls_pd_status       *pd_status)
+{
+    int rc;
+    rc = plugin->ofproto_ops_cls_apply(acl->want_pi, ofproto, bridgec_port,
+                                       interface_info, direction, pd_status);
+    VLOG_DBG("%s rc (%d)", __func__, rc);
+    return rc;
+}
+
+int
+call_ofproto_ops_cls_remove(struct acl                       *acl,
+                            struct port                      *bridgec_port,
+                            struct ofproto                   *ofproto,
+                            struct ops_cls_interface_info    *interface_info,
+                            enum ops_cls_direction           direction,
+                            struct ops_cls_pd_status         *pd_status)
+{
+    int rc;
+    rc = plugin->ofproto_ops_cls_remove(&acl->uuid, acl->name,
+                                        acl->type, ofproto,
+                                        bridgec_port, interface_info,
+                                        direction, pd_status);
+    VLOG_DBG("%s rc (%d)", __func__, rc);
+    return rc;
+}
+
+int
+call_ofproto_ops_cls_replace(struct acl                      *orig_acl,
+                             struct acl                      *new_acl,
+                             struct port                     *bridgec_port,
+                             struct ofproto                  *ofproto,
+                             struct ops_cls_interface_info   *interface_info,
+                             enum ops_cls_direction          direction,
+                             struct ops_cls_pd_status        *pd_status)
+{
+    int rc;
+    rc = plugin->ofproto_ops_cls_replace(&orig_acl->uuid, orig_acl->name,
+                                         new_acl->want_pi, ofproto,
+                                         bridgec_port, interface_info,
+                                         direction, pd_status);
+    VLOG_DBG("%s rc (%d)", __func__, rc);
+    return rc;
+}
+
+int
+call_ofproto_ops_cls_list_update(struct acl                     *acl,
+                                 struct ops_cls_pd_list_status  *status)
+{
+    int rc;
+    rc = plugin->ofproto_ops_cls_list_update(acl->want_pi, status);
+    VLOG_DBG("%s rc (%d)", __func__, rc);
+    return rc;
+}
+
+int
+call_ofproto_ops_cls_statistics_get(struct acl       *acl,
+                      struct port                    *bridgec_port,
+                      struct ofproto                 *ofproto,
+                      struct ops_cls_interface_info  *interface_info,
+                      enum ops_cls_direction         direction,
+                      struct ops_cls_statistics      *statistics,
+                      int                            num_entries,
+                      struct ops_cls_pd_list_status  *status)
+{
+    int rc;
+    rc = plugin->ofproto_ops_cls_statistics_get(&acl->uuid, acl->name,
+                                                acl->type, ofproto,
+                                                bridgec_port, interface_info,
+                                                direction, statistics,
+                                                num_entries, status);
+    VLOG_DBG("%s rc (%d)", __func__, rc);
+    return rc;
+}
+
+int
+call_ofproto_ops_cls_statistics_clear(struct acl      *acl,
+                      struct port                     *bridgec_port,
+                      struct ofproto                  *ofproto,
+                      struct ops_cls_interface_info   *interface_info,
+                      enum ops_cls_direction          direction,
+                      struct ops_cls_pd_list_status   *status)
+{
+    int rc;
+    rc = plugin->ofproto_ops_cls_statistics_clear(&acl->uuid, acl->name,
+                                                  acl->type, ofproto,
+                                                  bridgec_port,
+                                                  interface_info, direction,
+                                                  status);
+    VLOG_DBG("%s rc (%d)", __func__, rc);
+    return rc;
+}
+
+int
+call_ofproto_ops_cls_statistics_clear_all(
+                               struct ops_cls_pd_list_status    *status)
+{
+    int rc;
+    rc = plugin->ofproto_ops_cls_statistics_clear_all(status);
+    VLOG_DBG("%s rc (%d)", __func__, rc);
+    return rc;
+}
+
+void
+acl_ofproto_init()
+{
+    int rc;
+
+    rc = find_plugin_extension(OPS_CLS_ASIC_PLUGIN_INTERFACE_NAME,
+                               OPS_CLS_ASIC_PLUGIN_INTERFACE_MAJOR,
+                               OPS_CLS_ASIC_PLUGIN_INTERFACE_MINOR,
+                               &extension);
+    if (rc == 0) {
+        plugin = (struct ops_cls_plugin_interface *)
+                  extension->plugin_interface;
+        /* Initialize ACL logging */
+        if (plugin && plugin->ofproto_ops_cls_acl_log_pkt_register_cb) {
+            plugin->ofproto_ops_cls_acl_log_pkt_register_cb(
+                                                        &acl_log_pkt_data_set);
+        }
+    }
+}

vgribov/ops-classifierd

src/acl/switchd-plugin/acl.h
@@ -1,0 +1,61 @@
+/*
+ * Copyright (C) 2016 Hewlett Packard Enterprise Development LP
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at:
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef __SWITCHD__PLUGIN__ACL_H__
+#define __SWITCHD__PLUGIN__ACL_H__ 1
+
+#include <stdbool.h>
+#include "hmap.h"
+#include "uuid.h"
+#include "list.h"
+#include "ops-cls-asic-plugin.h"
+#include "reconfigure-blocks.h"
+
+struct classifier_list;
+struct ops_cls_list;
+
+/*************************************************************
+ * acl structures
+ *************************************************************/
+struct acl {
+    struct hmap_node   all_node_uuid;   /* In 'all_acls_by_uuid'. */
+
+    /* members with information "about" me */
+    struct uuid        uuid;
+    const char        *name;
+    enum ops_cls_type  type;
+
+    /* members for working with OVSDB */
+    const struct ovsrec_acl *ovsdb_row;
+    unsigned int       delete_seqno; /* mark/sweep to identify deleted */
+
+    /* members representing my cached PI state */
+    struct ovs_list acl_port_map;    /* List of struct acl_port_map. */
+    struct ops_cls_list *cfg_pi; /* List of acls user requested to
+                                    configure but not yet processed */
+};
+
+/*************************************************************
+ * acl lookup routines
+ *************************************************************/
+struct acl* acl_lookup_by_uuid(const struct uuid* uuid);
+
+/************************************************************
+ * Top level routine to check if ACL's need to reconfigure
+ ************************************************************/
+void acl_reconfigure_init(struct blk_params *blk_params);
+
+#endif  /* __SWITCHD__PLUGIN__ACL_H__ */

src/acl/switchd-plugin/acl_ofproto.c
@@ -1,0 +1,188 @@
+/*
+ * Copyright (C) 2016 Hewlett Packard Enterprise Development LP
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at:
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "acl_ofproto.h"
+#include <errno.h>
+#include <config.h>
+#include "plugin-extensions.h"
+#include "ops-cls-asic-plugin.h"
+#include "ofproto/ofproto-provider.h"
+#include "openvswitch/vlog.h"
+#include "acl.h"
+#include "acl_plugin.h"
+#include "acl_log.h"
+
+VLOG_DEFINE_THIS_MODULE(acl_switchd_plugin_ofproto);
+
+static struct plugin_extension_interface *extension = NULL;
+static struct ops_cls_plugin_interface *plugin = NULL;
+
+const char * const ops_cls_type_strings[] = {
+    "INVALID",
+    "ACL_V4",
+    "ACL_V6"
+};
+
+const char * const ops_cls_direction_strings[] = {
+    "INVALID",
+    "IN",
+    "OUT"
+};
+
+struct ops_cls_list*
+ops_cls_list_new(void)
+{
+    struct ops_cls_list *list = xzalloc(sizeof *list);
+    return list;
+}
+
+void
+ops_cls_list_delete(struct ops_cls_list *list)
+{
+    if (list) {
+        free(CONST_CAST(char*, list->list_name));
+        free(list->entries);
+        free(list);
+    }
+}
+
+int
+call_ofproto_ops_cls_apply(struct acl                     *acl,
+                           struct port                    *bridgec_port,
+                           struct ofproto                 *ofproto,
+                           struct ops_cls_interface_info  *interface_info,
+                           enum ops_cls_direction         direction,
+                           struct ops_cls_pd_status       *pd_status)
+{
+    int rc;
+    rc = plugin->ofproto_ops_cls_apply(acl->cfg_pi, ofproto, bridgec_port,
+                                       interface_info, direction, pd_status);
+    VLOG_DBG("%s rc (%d)", __func__, rc);
+    return rc;
+}
+
+int
+call_ofproto_ops_cls_remove(struct acl                       *acl,
+                            struct port                      *bridgec_port,
+                            struct ofproto                   *ofproto,
+                            struct ops_cls_interface_info    *interface_info,
+                            enum ops_cls_direction           direction,
+                            struct ops_cls_pd_status         *pd_status)
+{
+    int rc;
+    rc = plugin->ofproto_ops_cls_remove(&acl->uuid, acl->name,
+                                        acl->type, ofproto,
+                                        bridgec_port, interface_info,
+                                        direction, pd_status);
+    VLOG_DBG("%s rc (%d)", __func__, rc);
+    return rc;
+}
+
+int
+call_ofproto_ops_cls_replace(struct acl                      *orig_acl,
+                             struct acl                      *new_acl,
+                             struct port                     *bridgec_port,
+                             struct ofproto                  *ofproto,
+                             struct ops_cls_interface_info   *interface_info,
+                             enum ops_cls_direction          direction,
+                             struct ops_cls_pd_status        *pd_status)
+{
+    int rc;
+    rc = plugin->ofproto_ops_cls_replace(&orig_acl->uuid, orig_acl->name,
+                                         new_acl->cfg_pi, ofproto,
+                                         bridgec_port, interface_info,
+                                         direction, pd_status);
+    VLOG_DBG("%s rc (%d)", __func__, rc);
+    return rc;
+}
+
+int
+call_ofproto_ops_cls_list_update(struct acl                     *acl,
+                                 struct ops_cls_pd_list_status  *status)
+{
+    int rc;
+    rc = plugin->ofproto_ops_cls_list_update(acl->cfg_pi, status);
+    VLOG_DBG("%s rc (%d)", __func__, rc);
+    return rc;
+}
+
+int
+call_ofproto_ops_cls_statistics_get(struct acl       *acl,
+                      struct port                    *bridgec_port,
+                      struct ofproto                 *ofproto,
+                      struct ops_cls_interface_info  *interface_info,
+                      enum ops_cls_direction         direction,
+                      struct ops_cls_statistics      *statistics,
+                      int                            num_entries,
+                      struct ops_cls_pd_list_status  *status)
+{
+    int rc;
+    rc = plugin->ofproto_ops_cls_statistics_get(&acl->uuid, acl->name,
+                                                acl->type, ofproto,
+                                                bridgec_port, interface_info,
+                                                direction, statistics,
+                                                num_entries, status);
+    VLOG_DBG("%s rc (%d)", __func__, rc);
+    return rc;
+}
+
+int
+call_ofproto_ops_cls_statistics_clear(struct acl      *acl,
+                      struct port                     *bridgec_port,
+                      struct ofproto                  *ofproto,
+                      struct ops_cls_interface_info   *interface_info,
+                      enum ops_cls_direction          direction,
+                      struct ops_cls_pd_list_status   *status)
+{
+    int rc;
+    rc = plugin->ofproto_ops_cls_statistics_clear(&acl->uuid, acl->name,
+                                                  acl->type, ofproto,
+                                                  bridgec_port,
+                                                  interface_info, direction,
+                                                  status);
+    VLOG_DBG("%s rc (%d)", __func__, rc);
+    return rc;
+}
+
+int
+call_ofproto_ops_cls_statistics_clear_all(
+                               struct ops_cls_pd_list_status    *status)
+{
+    int rc;
+    rc = plugin->ofproto_ops_cls_statistics_clear_all(status);
+    VLOG_DBG("%s rc (%d)", __func__, rc);
+    return rc;
+}
+
+void
+acl_ofproto_init()
+{
+    int rc;
+
+    rc = find_plugin_extension(OPS_CLS_ASIC_PLUGIN_INTERFACE_NAME,
+                               OPS_CLS_ASIC_PLUGIN_INTERFACE_MAJOR,
+                               OPS_CLS_ASIC_PLUGIN_INTERFACE_MINOR,
+                               &extension);
+    if (rc == 0) {
+        plugin = (struct ops_cls_plugin_interface *)
+                  extension->plugin_interface;
+        /* Initialize ACL logging */
+        if (plugin && plugin->ofproto_ops_cls_acl_log_pkt_register_cb) {
+            plugin->ofproto_ops_cls_acl_log_pkt_register_cb(
+                                                        &acl_log_pkt_data_set);
+        }
+    }
+}