OpenSwitchNOS/openswitch-ops-classifierd

Update test with access list control cli max entries limit
This commit depends on ops-topology-lib-vtysh review: 11075

Change-Id: I40316a35138291a71d0afa8bac15c5d9e343d5a2
Signed-off-by: Shen Li <shen.li@hpe.com>

ops-tests/component/test_classifierd_ct_ace_parameters.py
@@ -357,15 +357,15 @@ def test_ace_parameters(topology, step):
     step('################ Command success. ###############')
 
     # TODO: Change exception library then add as negative test
-    # with pytest.raises(exceptions.TcamResourcesException):
-    # with ops1.libs.vtysh.ConfigAccessListIpTestname('test17') as ctx:
-    #     for i in range(1, 513):
-    #         ctx.deny(
-    #             '',
-    #             i, 'udp', '1.2.3.4/8', 'eq 4',
-    #             '5.6.7.8/24', 'eq 40')
+    with pytest.raises(exceptions.TcamResourcesException):
+        with ops1.libs.vtysh.ConfigAccessListIpTestname('test17') as ctx:
+            for i in range(1, 514):
+                ctx.deny(
+                    '',
+                    i, 'udp', '1.2.3.4/8', 'eq 4',
+                    '5.6.7.8/24', 'eq 40')
 
-    # test1_result = ops1('show run')
+    test1_result = ops1('show run')
 
     step('################ T18 Add ACE ###########')
     step('################ Resequence ACEs###############')
@@ -392,12 +392,12 @@ def test_ace_parameters(topology, step):
     step('################ Negative Test###############')
 
     # TODO: Change Exception library to proper exception
-    # with pytest.raises(
-    #             exceptions.ResequenceNumberException):
-    #         with ops1.libs.vtysh.Configure() as ctx:
-    #             ctx.access_list_ip_resequence('test18', '500', '10')
+    with pytest.raises(
+                exceptions.ResequenceNumberException):
+        with ops1.libs.vtysh.Configure() as ctx:
+            ctx.access_list_ip_resequence('test18', '4294967295', '10')
 
-    # test1_result = ops1('show run')
+    test1_result = ops1('show run')
 
     step('################ T20 Replace deny ACE ###########')
     step('################ with Permit ACE ###############')
@@ -426,16 +426,16 @@ def test_ace_parameters(topology, step):
 
     step('################ T22 Remove ACE ###########')
     step('################ Positive Test ###############')
-    # with ops1.libs.vtysh.ConfigAccessListIpTestname('test17') as ctx:
-    #     ctx.no('401')
+    with ops1.libs.vtysh.ConfigAccessListIpTestname('test17') as ctx:
+        ctx.no('401')
 
-    # test1_result = ops1('show run')
+    test1_result = ops1('show run')
 
-    # assert search(
-    #    ''
-    #    r'(?!401\s+permit\s+udp\s+[0-9]\.[0-9]\.[0-9]\.[0-9]/255\.0\.0\.0'
-    #    '\s+eq\s+4\s+[0-9]\.[0-9]\.[0-9]\.[0-9]/255\.255\.255\.0'
-    #    '\s+eq\s+40)'.format(
-    #                                      **locals()
-    #                                    ), test1_result
-    # )
+    assert search(
+       ''
+       r'(?!401\s+permit\s+udp\s+[0-9]\.[0-9]\.[0-9]\.[0-9]/255\.0\.0\.0'
+       '\s+eq\s+4\s+[0-9]\.[0-9]\.[0-9]\.[0-9]/255\.255\.255\.0'
+       '\s+eq\s+40)'.format(
+                                         **locals()
+                                       ), test1_result
+    )

vgribov/ops-classifierd

ops-tests/component/test_classifierd_ct_ace_parameters.py
@@ -356,16 +356,15 @@ def test_ace_parameters(topology, step):
     step('################ Allowed ACE ###############')
     step('################ Command success. ###############')
 
-    # TODO: Change exception library then add as negative test
-    # with pytest.raises(exceptions.TcamResourcesException):
-    # with ops1.libs.vtysh.ConfigAccessListIpTestname('test17') as ctx:
-    #     for i in range(1, 513):
-    #         ctx.deny(
-    #             '',
-    #             i, 'udp', '1.2.3.4/8', 'eq 4',
-    #             '5.6.7.8/24', 'eq 40')
+    with pytest.raises(exceptions.MaxACEsException):
+        with ops1.libs.vtysh.ConfigAccessListIpTestname('test17') as ctx:
+            for i in range(1, 514):
+                ctx.deny(
+                    '',
+                    i, 'udp', '1.2.3.4/8', 'eq 4',
+                    '5.6.7.8/24', 'eq 40')
 
-    # test1_result = ops1('show run')
+    test1_result = ops1('show run')
 
     step('################ T18 Add ACE ###########')
     step('################ Resequence ACEs###############')
@@ -391,13 +390,12 @@ def test_ace_parameters(topology, step):
     step('################ Resequence ACEs###############')
     step('################ Negative Test###############')
 
-    # TODO: Change Exception library to proper exception
-    # with pytest.raises(
-    #             exceptions.ResequenceNumberException):
-    #         with ops1.libs.vtysh.Configure() as ctx:
-    #             ctx.access_list_ip_resequence('test18', '500', '10')
+    with pytest.raises(
+                exceptions.ResequenceNumberException):
+        with ops1.libs.vtysh.Configure() as ctx:
+            ctx.access_list_ip_resequence('test18', '4294967295', '10')
 
-    # test1_result = ops1('show run')
+    test1_result = ops1('show run')
 
     step('################ T20 Replace deny ACE ###########')
     step('################ with Permit ACE ###############')
@@ -426,16 +424,16 @@ def test_ace_parameters(topology, step):
 
     step('################ T22 Remove ACE ###########')
     step('################ Positive Test ###############')
-    # with ops1.libs.vtysh.ConfigAccessListIpTestname('test17') as ctx:
-    #     ctx.no('401')
+    with ops1.libs.vtysh.ConfigAccessListIpTestname('test17') as ctx:
+        ctx.no('401')
 
-    # test1_result = ops1('show run')
+    test1_result = ops1('show run')
 
-    # assert search(
-    #    ''
-    #    r'(?!401\s+permit\s+udp\s+[0-9]\.[0-9]\.[0-9]\.[0-9]/255\.0\.0\.0'
-    #    '\s+eq\s+4\s+[0-9]\.[0-9]\.[0-9]\.[0-9]/255\.255\.255\.0'
-    #    '\s+eq\s+40)'.format(
-    #                                      **locals()
-    #                                    ), test1_result
-    # )
+    assert search(
+       ''
+       r'(?!401\s+permit\s+udp\s+[0-9]\.[0-9]\.[0-9]\.[0-9]/255\.0\.0\.0'
+       '\s+eq\s+4\s+[0-9]\.[0-9]\.[0-9]\.[0-9]/255\.255\.255\.0'
+       '\s+eq\s+40)'.format(
+                                         **locals()
+                                       ), test1_result
+    )